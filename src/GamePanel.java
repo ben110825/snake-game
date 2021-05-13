b1cabf4a880d1ba5f55fd89727e74c1a8af0f073
ben110825 <ben110825@gmail.com>
2021-05-13 15:08:32 +0800

    finish game

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,9 +1,12 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.event.*;
+import java.util.Date;
 import java.util.Random;
-import java.util.Timer;
+import javax.swing.Timer;
 import java.util.TimerTask;
 
 import javax.swing.JPanel;
@@ -23,6 +26,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	int appleY;
 	char direction = 'R';
 	boolean running = false;
+	boolean gameOn = false;
 	Timer timer;
 	Random random;
 
@@ -38,46 +42,161 @@ public class GamePanel extends JPanel implements ActionListener{
 	public void startGame() {
 		newApple();
 		running = true;
-		timer = new Timer();
-		timer.schedule(new DateTask(), 3000);
-		repaint();
+		timer = new Timer(DELAY, this);
+        timer.start();
+        
 	}
+	
+	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		draw(g);
 	}
 	public void draw(Graphics g) {
-		for(int i=0;i<SCREEN_WIDTH/UNIT_SIZE;i++) {
-			 g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-			 g.drawLine(0, i*UNIT_SIZE,  SCREEN_WIDTH, i*UNIT_SIZE);
-
+		if(running) {
+//			for(int i=0;i<SCREEN_WIDTH/UNIT_SIZE;i++) {
+//				 g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+//				 g.drawLine(0, i*UNIT_SIZE,  SCREEN_WIDTH, i*UNIT_SIZE);
+//	
+//			}
+			g.setColor(Color.red);
+			g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+			
+			for(int i=0; i<bodyParts;i++) {
+				if(i==0) {
+					g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				}
+				else {
+					g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				}
+			}
+			g.setColor(Color.yellow);
+			g.setFont(new Font(Font.DIALOG,Font.BOLD,30));
+			FontMetrics metrics = getFontMetrics(g.getFont()) ;
+			g.drawString("Score: "+applesEaten, (SCREEN_WIDTH-metrics.stringWidth("Score: "+applesEaten))/2, SCREEN_HEIGHT/10);
+		}
+		else {
+		
+			gameOver(g);
 		}
 	}
 	public void newApple() {
-		
+		appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+		appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+
 	}
 	public void move() {
-		
+		for(int i=bodyParts;i>0;i--) {
+			x[i] = x[i-1];
+			y[i] = y[i-1];
+		}
+		switch(direction) {
+			case 'U':
+				y[0] = y[0]-UNIT_SIZE;
+				break;
+			case 'D':
+				y[0] = y[0]+UNIT_SIZE;
+				break;
+			case 'L':
+				x[0] = x[0]-UNIT_SIZE;
+				break;
+			case 'R':
+				x[0] = x[0]+UNIT_SIZE;
+				break;
+			
+		}
+	}
+	public void pause() {
+		gameOn = true;
+		timer.stop();
+	}
+	public void resume() {
+		gameOn = false;
+		timer.start();
 	}
 	public void checkApple() {
-		
+		if((x[0]==appleX)&&(y[0]==appleY)) {
+			bodyParts++;
+			applesEaten++;
+			newApple();
+		}
 	}
 	public void checkCollision() {
+		for(int i=bodyParts;i>0;i--) {
+			if((x[0] == x[i]) && (y[0] == y[i])) {
+				running = false;
+			}
+		}
 		
+		if(x[0] < 0) {
+			running = false;
+		}
+		if(x[0] > SCREEN_WIDTH) {
+			running = false;
+		}
+		if(y[0] < 0) {
+			running = false;
+		}
+		if(y[0] > SCREEN_HEIGHT) {
+			running = false;
+		}
+		
+		if(!running) {
+			timer.stop();
+		}
 	}
 	public void gameOver(Graphics g) {
-		
+		g.setColor(Color.red);
+		g.setFont(new Font(Font.DIALOG,Font.BOLD,75));
+		FontMetrics metrics = getFontMetrics(g.getFont()) ;
+		g.drawString("GameOver", (SCREEN_WIDTH-metrics.stringWidth("GameOver"))/2, SCREEN_HEIGHT/2);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		if(running) {
+			move();
+			checkApple();
+			checkCollision();
+		}
+		repaint();
 	}
 	public class MyKeyAdapter extends KeyAdapter {
 		@Override
 		public void keyPressed(KeyEvent e) {
-			
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				if(direction != 'R') {
+					direction = 'L';
+				}
+				break;
+			case KeyEvent.VK_RIGHT:
+				if(direction != 'L') {
+					direction = 'R';
+				}
+				break;
+			case KeyEvent.VK_UP:
+				if(direction != 'D') {
+					direction = 'U';
+				}
+				break;
+			case KeyEvent.VK_DOWN:
+				if(direction != 'U') {
+					direction = 'D';
+				}
+				break;
+			case KeyEvent.VK_ESCAPE	:
+				if(!gameOn) {
+					pause();
+				}
+				else{
+					resume();
+				}
+				break;
+			}
 		}
 	}
+	
 
 }
